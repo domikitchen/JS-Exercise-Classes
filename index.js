2d09c26ea82e1fc4cdc69bc766ccc1494aad9eee
Dominique K Kitchenakow <domikitchen@users.noreply.github.com>
2020-05-21 16:36:59 -0500

    skipped task 2 for the time being and completed task 3 on task 4

--- index.js
+++ index.js
@@ -79,10 +79,23 @@ class Car {
   constructor(model, milesPerGallon) {
     this.model = model;
     this.milesPerGallon = milesPerGallon;
-    let tank = 0;
-    let odometer = 0;
+    this.tank = 0;
+    this.odometer = 0;
+  }
+  fill(gallons) {
+    this.tank = this.tank + gallons;
+  }
+  drive(distance) {
+    if (this.tank > 0) {
+      this.odometer = this.odometer + distance;
+      this.tank = distance / this.milesPerGallon;
+    }
+    else {
+      return `I ran out of fuel at ${this.odometer} miles!`;
+    }
   }
 }
+
 
 /*
   TASK 3
@@ -97,7 +110,14 @@ class Car {
         + {name} and {location} of course come from the instance's own properties.
 */
 class Lambdasian {
-
+  constructor(atty) {
+    this.name = atty.name;
+    this.age = atty.age;
+    this.location = atty.location;
+  }
+  speak() {
+    return `Hello my name is ${this.name}, I am from ${this.location}`;
+  }
 }
 
 /*
@@ -114,8 +134,11 @@ class Lambdasian {
         + `demo` receives a `subject` string as an argument and returns the phrase 'Today we are learning about {subject}' where subject is the param passed in.
         + `grade` receives a `student` object and a `subject` string as arguments and returns '{student.name} receives a perfect score on {subject}'
 */
-class Instructor {
-
+class Instructor extends Lambdasian{
+  constructor(atty) {
+    super(atty);
+    this.specialty = atty.specialty;
+  }
 }
 
 /*
